59b61ec99f100488998f75a3170e138e09e405ba
Hiroyuki Onishi <onishi.hiroyuki@gmail.com>
2014-02-28 11:05:02 +0900

    Initial Commit

---
+++ OCTestTemplateTests/OCTestTemplateTests.m
@@ -0,0 +1,34 @@
+//
+//  OCTestTemplateTests.m
+//  OCTestTemplateTests
+//
+//  Created by Hiroyuki Onishi on 2/28/14.
+//  Copyright (c) 2014 Hiroyuki Onishi. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface OCTestTemplateTests : XCTestCase
+
+@end
+
+@implementation OCTestTemplateTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
